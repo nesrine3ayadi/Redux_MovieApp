f14878b358f1af40a48b1a10c9a63b5aa8a75741
nesrine3ayadi <nesrine3ayadi@gmail.com>
2020-04-24 09:59:39 +0100

    Update Data.js

--- src/Constant/Data.js
+++ src/Constant/Data.js
@@ -15,6 +15,7 @@ export const MoviesData = [
     rating: 2,
     img: "https://fr.web.img4.acsta.net/medias/nmedia/18/35/78/09/18648048.jpg",
     edit: false,
+    description:"Alors qu'un ennemi de longue date, Lex Luthor, tente de l'affaiblir en lui dérobant tous ses superpouvoirs, Superman doit faire face à un problème d'un autre genre : Lois Lane, la femme qu'il aime, est partie mener une nouvelle vie loin de lui. Mais l'a-t-elle vraiment oublié ?"
   },
   {
     id: 3,
@@ -22,6 +23,7 @@ export const MoviesData = [
     rating: 3,
     img: "https://fr.web.img6.acsta.net/medias/nmedia/18/83/19/35/20158079.jpg",
     edit: false,
+    description:"Abandonné par ses parents lorsqu’il était enfant, Peter Parker a été élevé par son oncle Ben et sa tante May. Il est aujourd’hui au lycée, mais il a du mal à s’intégrer. Comme la plupart des adolescents de son âge, Peter essaie de comprendre qui il est et d’accepter son parcours. Amoureux pour la première fois, lui et Gwen Stacy découvrent les sentiments, l’engagement et les secrets. En retrouvant une mystérieuse mallette ayant appartenu à son père, Peter entame une quête pour élucider la disparition de ses parents, ce qui le conduit rapidement à Oscorp et au laboratoire du docteur Curt Connors, l’ancien associé de son père. Spider-Man va bientôt se retrouver face au Lézard, l’alter ego de Connors. En décidant d’utiliser ses pouvoirs, il va choisir son destin…"
   },
   {
     id: 4,
@@ -29,5 +31,6 @@ export const MoviesData = [
     rating: 4,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ4MYpC0CWIQluyegi1IdWXXlisMuc2ednGjbxGuTdfSHUjo75W&usqp=CAU",
     edit: false,
+    description:"Faites la connaissance de WALL•E (prononcez'Walli') : WALL•E est le dernier être sur Terre et s'avère être un... petit robot ! 700 ans plus tôt, l'humanité a déserté notre planète laissant à cette incroyable petite machine le soin de nettoyer la Terre. Mais au bout de ces longues années, WALL•E a développé un petit défaut technique : une forte personnalité. Extrêmement curieux, très indiscret, il est surtout un peu trop seul..."
   },
 ];
